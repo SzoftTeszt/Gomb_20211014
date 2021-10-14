ead0ad2051827ea9657a312bae8d1711cae4cb9e
Attila <szoftteszt2020@gmail.com>
2021-10-14 10:30:39 +0200

    gomb+LED talan

--- gomb.ino
+++ gomb.ino
@@ -1,4 +1,6 @@
 int gomb =3;
+int fenyero =50;
+int intenzitas=0;
 void setup() {
   Serial.begin(9600);
   pinMode(3, INPUT_PULLUP);
@@ -10,8 +12,13 @@ void loop() {
   if (digitalRead(3)==0)
   {
    delay(100);
-   if (digitalRead(3)==0) {Serial.println("Megnyomtak! Üdv, a Gomb!");}
-   // delay(millisec);  
+   if (digitalRead(3)==0) {
+    Serial.println("Megnyomtak! Üdv, a Gomb!");
+    fenyero+=10;
+    if (fenyero>=100) fenyero=0;
+    }
+    intenzitas = map(fenyero,0,100,0, 255);
+    analogWrite(2,intenzitas);
   }
   
 }
